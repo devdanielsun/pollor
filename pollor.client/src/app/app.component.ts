b80599b8106f8f7e5de97c4b27fd82b752ab3e99
Daniël Geerts <daniel.geerts@live.nl>
2024-01-05 02:00:42 +0100

    Add header and footer and bootstrap

--- pollor.client/src/app/app.component.ts
+++ pollor.client/src/app/app.component.ts
@@ -1,38 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
-import { environment } from '../environments/environment';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public appVersion: String = environment.appVersion;
-  public baseApiUrl: String = this.apiService.getBaseUrl();
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.apiService.get<WeatherForecast[]>('weatherforecast')
-      .subscribe({
-        next: (response) => this.forecasts = response,
-        error: (error) => console.error(error),
-        //complete: () => { }
-      });
-  }
-
+export class AppComponent {
   title = 'pollor.client';
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
 }
