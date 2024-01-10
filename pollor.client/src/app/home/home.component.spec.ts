f43775a7eb2b14f47b56262709f163a174387b48
devdanielsun <daniel.geerts@live.nl>
2024-01-10 17:43:31 +0100

    Remove old test

--- pollor.client/src/app/home/home.component.spec.ts
+++ pollor.client/src/app/home/home.component.spec.ts
@@ -13,7 +13,7 @@ describe('HomeComponent', () => {
       declarations: [HomeComponent],
       imports: [HttpClientTestingModule]
     }).compileComponents();
-    
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -26,20 +26,5 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should retrieve weather forecasts from the server', () => {
-    const mockForecasts = [
-      { date: '2021-10-01', temperatureC: 20, temperatureF: 68, summary: 'Mild' },
-      { date: '2021-10-02', temperatureC: 25, temperatureF: 77, summary: 'Warm' }
-    ];
-
-    component.ngOnInit();
-
-    const req = httpMock.expectOne('/weatherforecast');
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockForecasts);
-
-    expect(component.forecasts).toEqual(mockForecasts);
   });
 });
