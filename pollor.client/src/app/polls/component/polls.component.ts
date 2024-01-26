67db2c5413320c05af5c7618867783a46cbce627
devdanielsun <daniel.geerts@live.nl>
2024-01-26 15:34:34 +0100

    Update to fit new responces

--- pollor.client/src/app/polls/component/polls.component.ts
+++ pollor.client/src/app/polls/component/polls.component.ts
@@ -12,19 +12,13 @@ import { IVotes } from '../votes.interface';
 })
 export class PollsComponent {
   public polls: IPolls[] = [];
-  public answers: IAnswers[] = [];
-  public votes: IVotes[] = [];
 
   public pollsLoaded: boolean = false;
-  public answersLoaded: boolean = false;
-  public votesLoaded: boolean = false;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.getPolls();
-    this.getAnswers();
-    this.getVotes();
   }
 
   getPolls() {
@@ -33,30 +27,6 @@ export class PollsComponent {
         next: (response) => {
           this.polls = response;
           this.pollsLoaded = true;
-        },
-        error: (error) => console.error(error),
-        //complete: () => { }
-      });
-  }
-
-  getAnswers() {
-    this.apiService.get<IAnswers[]>('answers')
-      .subscribe({
-        next: (response) => {
-          this.answers = response;
-          this.answersLoaded = true;
-        },
-        error: (error) => console.error(error),
-        //complete: () => { }
-      });
-  }
-
-  getVotes() {
-    this.apiService.get<IVotes[]>('votes')
-      .subscribe({
-        next: (response) => {
-          this.votes = response;
-          this.votesLoaded = true;
         },
         error: (error) => console.error(error),
         //complete: () => { }
--- pollor.client/src/app/polls/polls.interface.ts
+++ pollor.client/src/app/polls/polls.interface.ts
@@ -1,7 +1,10 @@
+import { IAnswers } from "./answers.interface";
+
 export interface IPolls {
   id: string;
   user_id: number;
   question: number;
   ending_date: Date;
   created_at: Date;
+  answers: IAnswers[];
 }
