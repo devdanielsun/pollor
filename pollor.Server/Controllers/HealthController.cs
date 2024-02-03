8582ab67b47de9fcb8ba8262a8c6f622c9d30c3d
devdanielsun <daniel.geerts@live.nl>
2024-02-03 22:15:16 +0100

    Add api get to check if server is online

---
+++ pollor.Server/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pollor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok("healthy");
+        }
+    }
+}
