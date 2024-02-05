331c450f6d09cb6e14c4547454e1e06ca6505d7c
devdanielsun <daniel.geerts@live.nl>
2024-02-05 05:28:55 +0100

    Update sql migration with datetime2

--- database/migration.sql
+++ database/migration.sql
@@ -19,7 +19,7 @@ CREATE TABLE [dbo].[users](
 	[first_name] [nvarchar](64) NULL,
 	[last_name] [nvarchar](64) NULL,
 	[role] [nvarchar](32) NULL DEFAULT 'Basic',
-	[created_at] [datetime] NOT NULL,
+	[created_at] [datetime2](7) NOT NULL,
 	CONSTRAINT PK_users PRIMARY KEY NONCLUSTERED (id),
 	CONSTRAINT UC_Users UNIQUE (id,emailaddress,username)
 ) ON [PRIMARY]
@@ -29,8 +29,8 @@ CREATE TABLE [dbo].[polls](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[user_id] [int] NOT NULL,
 	[question] [nvarchar](512) NOT NULL,
-	[ending_date] [datetime] NOT NULL,
-	[created_at] [datetime] NOT NULL,
+	[ending_date] [datetime2](7) NOT NULL,
+	[created_at] [datetime2](7) NOT NULL,
 	CONSTRAINT PK_polls PRIMARY KEY NONCLUSTERED (id),
 	CONSTRAINT FK_poll_user FOREIGN KEY (user_id)
       REFERENCES users (id)
@@ -45,7 +45,7 @@ CREATE TABLE [dbo].[answers](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[poll_id] [int] NOT NULL,
 	[poll_answer] [nvarchar](256) NOT NULL,
-	[created_at] [datetime] NOT NULL,
+	[created_at] [datetime2](7) NOT NULL,
 	CONSTRAINT PK_answers PRIMARY KEY NONCLUSTERED (id),
 	CONSTRAINT FK_answer_poll FOREIGN KEY (poll_id)
       REFERENCES polls (id)
@@ -60,8 +60,8 @@ CREATE TABLE [dbo].[votes](
 	[ipv4_address] [varbinary](4) NULL,
 	[ipv6_address] [varbinary](16) NULL,
 	[mac_address] [char](12) NULL,
-	[voted_at] [datetime] NOT NULL,
-	[created_at] [datetime] NOT NULL,
+	[voted_at] [datetime2](7) NOT NULL,
+	[created_at] [datetime2](7) NOT NULL,
 	CONSTRAINT PK_votes PRIMARY KEY NONCLUSTERED (id),
 	CONSTRAINT FK_vote_answer FOREIGN KEY (answer_id)
       REFERENCES answers (id)
