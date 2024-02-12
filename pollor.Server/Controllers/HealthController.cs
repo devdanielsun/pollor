706997fc43f1c81457317526a359836b4d3492fa
devdanielsun <daniel.geerts@live.nl>
2024-02-12 21:26:17 +0100

    Make api/Health call heavier

--- pollor.Server/Controllers/HealthController.cs
+++ pollor.Server/Controllers/HealthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+using pollor.Server.Models;
+using pollor.Server.Services;
 
 namespace pollor.Server.Controllers
 {
@@ -6,15 +9,43 @@ namespace pollor.Server.Controllers
     [ApiController]
     public class HealthController : ControllerBase
     {
+
+        private readonly ILogger<PollsController> _logger;
+
+        public HealthController(ILogger<PollsController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult GetHealth()
         {
-            var healthyObject = new {
-                healthy = "OK",
-                ping = "Ping successful"
-            };
+            int usersSize = 0;
+            int pollsSize = 0;
+            int answersSize = 0;
+            int votesSize = 0;
 
-            return Ok(healthyObject);
+            try {
+                using (var context = new PollorDbContext()) {
+                    usersSize = context.Users.Count();
+                    pollsSize = context.Polls.Count();
+                    answersSize = context.Answers.Count();
+                    votesSize = context.Votes.Count();
+                }
+            }
+            catch(Exception ex) {
+                _logger.LogError(ex.Message);
+                return StatusCode(500, new { message = ex.Message});
+            }
+
+            return Ok(new {
+                healthy = "OK",
+                ping = "Ping successful",
+                usersSize,
+                pollsSize,
+                answersSize,
+                votesSize,
+            });
         }
     }
 }
